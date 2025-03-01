4993d2d69a13496a14886285c81d8a50c735e79b
Wormfryes <wormfryes@hotmail.com>
2025-02-28 22:05:04 -0300

    Update main.py

--- website/main.py
+++ website/main.py
@@ -1,1 +1,18 @@
-# Coming Soon.
+from flask import Flask
+app = Flask(__name__)
+
+with open("index.html", "r") as page1:
+    indexpg = page1.read()
+with open("index.html", "r") as page2:
+    downloadpg = page2.read()
+
+@app.route('/')
+def home():
+    return indexpg;
+
+@app.route('/download')
+def home():
+    return downloadpg;
+
+if __name__ == '__main__' :
+    app.run(debug=True)
