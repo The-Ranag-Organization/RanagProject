dc2fe79d7b8cd7781c2d40aa5a2224260d23674f
Wormfryes <wormfryes@hotmail.com>
2025-02-28 22:28:37 -0300

    Update main.py

--- website/main.py
+++ website/main.py
@@ -14,5 +14,5 @@ def home():
 def download():
     return downloadpg;
 
-if __name__ == '__main__' :
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000, debug=True)
